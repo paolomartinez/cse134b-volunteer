83bd55254dd77dccb97aaea9a827a68b3c90622b
PJ Martinez <pem001@ucsd.edu>
2018-03-05 13:54:30 -0800

    added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+ReactDOM.render(
+    <App />, document.getElementById('root')
+);
+*/
+ReactDOM.render(
+    <HashRouter>
+        <App/>
+    </HashRouter>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
